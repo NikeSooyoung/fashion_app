70f04c47a88f9ff203e301273f5c77f12f8eb2a8
sooyoung jang <sooyoung.jang@nike.com>
2022-04-22 12:20:06 +0900

    first commit

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+    var shopswiper = new Swiper('.shop_products', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 2.5,
+        spaceBetween: 25
+    });
+    var categoryswiper = new Swiper('.category_slider_wrap', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        pagination: {
+            el: '.category_slider_wrap .pager',
+            type: 'bullets',
+            clickable:true
+        }
+    });
+    var detailSwiper = new Swiper('.details_pic_slides', {
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 1.2,
+        spaceBetween: 25.
+        pagination: {
+            el: '.details_pic_slides_wrapper .pager',
+            type: 'bullets',
+            clickable:true
+        }
+    });
+    //Aside Navigation
+    var asideOpenBtn = $('.menu_icon_left'),
+    aside = $('aside'),
+    asideCloseBtn = aside.find('close');
+
+    asideOpenBtn.click(function(){
+        aside.animate({left:0}, 300);
+    });
+    asideCloseBtn.click(function(){
+        aside.animate({left:'-100%'}, 300);
+    });
+    
+}); //document readt
